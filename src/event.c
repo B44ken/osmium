24172f8714c614e276b4cdb11afd1f6aaf26ec1f
b44ken <bradboratto@gmail.com>
2022-05-28 23:48:18 -0400

    windows patches

--- src/event.c
+++ src/event.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tab.h"
 #include "draw.h"
 #include "file.h"
--- src/h/tab.h
+++ src/h/tab.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 typedef struct {
     FILE* file;
     char* name;
